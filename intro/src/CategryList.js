1aa660f15e5c1dbe1ac60a03e24b3be8226c989f
= <tunay.ozdemir@hotmail.com>
2021-04-25 01:44:48 +0300

    https://github.com/typicode/json-server ile api den veri çekme

--- intro/src/CategryList.js
+++ intro/src/CategryList.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react'
 import { ListGroup, ListGroupItem } from 'reactstrap'
-
 export default class CategryList extends Component {
 
-  // contructor'ın olmasına gerek yok 
-
-
     state = {
-      categories: [
-        { categoryId: 1, categoryName: "Beverages" },
-        { categoryId: 2, categoryName: "Condiments" }
-      ],
+      categories: [],
+    }
+
+    // Component yerleşti anlamına gelir.life cycle
+    // .map sadece array 'e uygulanır
+    componentDidMount(){
+      this.getCategories();
+    }
+
+    getCategories = () => {
+      fetch("http://localhost:3000/categories")
+      .then(response=>response.json())
+      .then(data=>this.setState({categories:data}));
     }
   
-
   render() {
     return (
       <div>
