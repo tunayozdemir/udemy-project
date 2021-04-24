9326b64a44d04b84f5622be832d73593d6651ee9
= <tunay.ozdemir@hotmail.com>
2021-04-25 02:54:35 +0300

    hangi kategoriye tıklanırsa onu mavi yapmak. Şartlı ifadeler

--- intro/src/CategryList.js
+++ intro/src/CategryList.js
@@ -2,32 +2,33 @@ import React, { Component } from 'react'
 import { ListGroup, ListGroupItem } from 'reactstrap'
 export default class CategryList extends Component {
 
-    state = {
-      categories: [],
-    }
+  state = {
+    categories: [],
+  }
 
-    // Component yerleşti anlamına gelir.life cycle
-    // .map sadece array 'e uygulanır
-    componentDidMount(){
-      this.getCategories();
-    }
+  // Component yerleşti anlamına gelir.life cycle
+  // .map sadece array 'e uygulanır
+  componentDidMount() {
+    this.getCategories();
+  }
 
-    getCategories = () => {
-      fetch("http://localhost:3000/categories")
-      .then(response=>response.json())
-      .then(data=>this.setState({categories:data}));
-    }
-  
+  getCategories = () => {
+    fetch("http://localhost:3000/categories")
+      .then(response => response.json())
+      .then(data => this.setState({ categories: data }));
+  }
+
   render() {
     return (
       <div>
         <h5>{this.props.info.title}</h5>
-        <ListGroup>
+        <ListGroup >
           {this.state.categories.map(category => (
             <ListGroupItem
+              active={category.categoryName === this.props.currentCategory ? true : false}
               onClick={() => this.props.changeCategory(category)}
               key={category.id}
-              >
+            >
               {category.categoryName}
             </ListGroupItem>
           ))
